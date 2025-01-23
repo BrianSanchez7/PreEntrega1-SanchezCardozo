0c55ba525675e4993e3170672d2e0b0bfcabbc2b
Brian Sanchez Cardozo <88352116+BrianSanchez7@users.noreply.github.com>
2025-01-23 14:03:08 -0300

    Se agrega boton en checkout para volver al carrito y se estilan

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -8,49 +8,56 @@ const Cart = () => {
   const { cart, totalPrice, deleteProductById, deleteCart } =
     useContext(CartContext);
 
-    const formatedPrice = new Intl.NumberFormat('es-AR', {
-      style: 'currency',
-      currency: 'ARS',
-      maximumSignificantDigits: 3,
-    })
+  const formatedPrice = new Intl.NumberFormat("es-AR", {
+    style: "currency",
+    currency: "ARS",
+    maximumSignificantDigits: 3,
+  });
 
   //Early Return en caso de no tener items en el carrito
   if (cart.length === 0) {
     return (
       <div className="empty-cart-container">
         <h2>El carrito está vacio 🙁</h2>
-        <Link to="/" className="button-detail">Volver al Inicio</Link>
+        <Link to="/" className="button-detail">
+          Volver al Inicio
+        </Link>
       </div>
     );
   }
 
   return (
     <div className="cart-container">
+      <h2 style={{ padding: "1em", marginBottom: "0.3em", fontWeight: "500" }}>
+        Carrito de compras
+      </h2>
       <ul>
         {cart.map((productCart) => (
           <div className="cart-detail" key={productCart.id}>
-              <div className="cart-item">
-                <img src={productCart.image} width={150} alt="" />
-                <p>{productCart.name}</p>
-                <p>Cantidad: {productCart.quantity}</p>
-                <p>Precio Unitario: {formatedPrice.format(productCart.price)}</p>
-                <button
-                  className="button-delete"
-                  onClick={() => deleteProductById(productCart.id)}
-                >
-                  <MdOutlineDeleteForever className="trash-icon" />
-                </button>
-              </div>
+            <div className="cart-item">
+              <img src={productCart.image} width={150} alt="" />
+              <p>{productCart.name}</p>
+              <p>Cantidad: {productCart.quantity}</p>
+              <p>Precio Unitario: {formatedPrice.format(productCart.price)}</p>
+              <button
+                className="button-delete"
+                onClick={() => deleteProductById(productCart.id)}
+              >
+                <MdOutlineDeleteForever className="trash-icon" />
+              </button>
+            </div>
           </div>
         ))}
       </ul>
       <div className="finish-buy-container">
         <h2>Precio Total: {totalPrice()}</h2>
         <div>
-        <Link to ="/checkout" className="button-cart finish">Finalizar Compra</Link>
-        <button className="button-cart delete" onClick={deleteCart}>
-          Vaciar Carrito
-        </button>
+          <Link to="/checkout" className="button-cart finish">
+            Finalizar Compra
+          </Link>
+          <button className="button-cart delete" onClick={deleteCart}>
+            Vaciar Carrito
+          </button>
         </div>
       </div>
     </div>
--- src/components/Checkout/FormCheckout.jsx
+++ src/components/Checkout/FormCheckout.jsx
@@ -1,46 +1,53 @@
-import "./checkout.css"
+import { Link } from "react-router-dom";
+import "./checkout.css";
 
-const FormCheckout = ({dataForm, handleChangeInput, handleSubmitForm}) => {
+const FormCheckout = ({ dataForm, handleChangeInput, handleSubmitForm }) => {
   return (
     <form onSubmit={handleSubmitForm} className="form-container">
-        <label>Nombre Completo</label>
-        <input
-          type="text"
-          value={dataForm.fullname}
-          name="fullname"
-          onChange={handleChangeInput}
-          placeholder="Ingrese su nombre completo"
-        />
+      <label>Nombre Completo</label>
+      <input
+        type="text"
+        value={dataForm.fullname}
+        name="fullname"
+        onChange={handleChangeInput}
+        placeholder="Ingrese su nombre completo"
+      />
 
-        <label>Teléfono</label>
-        <input
-          type="number"
-          value={dataForm.phone}
-          name="phone"
-          onChange={handleChangeInput}
-          placeholder="Ingrese su teléfono celular"
-        />
+      <label>Teléfono</label>
+      <input
+        type="number"
+        value={dataForm.phone}
+        name="phone"
+        onChange={handleChangeInput}
+        placeholder="Ingrese su teléfono celular"
+      />
 
-        <label>Email</label>
-        <input
-          type="email"
-          value={dataForm.email}
-          name="email"
-          onChange={handleChangeInput}
-          placeholder="Ingrese su correo electrónico"
-        />
-        <label>Reingrese su Email</label>
-        <input
-          type="email"
-          value={dataForm.emailCopy}
-          name="emailCopy"
-          onChange={handleChangeInput}
-          placeholder="Reingrese su correo electrónico"
-        />
+      <label>Email</label>
+      <input
+        type="email"
+        value={dataForm.email}
+        name="email"
+        onChange={handleChangeInput}
+        placeholder="Ingrese su correo electrónico"
+      />
+      <label>Reingrese su Email</label>
+      <input
+        type="email"
+        value={dataForm.emailCopy}
+        name="emailCopy"
+        onChange={handleChangeInput}
+        placeholder="Reingrese su correo electrónico"
+      />
+      <div className="checkout-button-container">
+        <Link to="/cart" className="order-button">
+          Volver al carrito
+        </Link>
+        <button type="submit" className="order-button">
+          Enviar Orden
+        </button>
+      </div>
+    </form>
+  );
+};
 
-        <button type="submit" className="order-button">Enviar Orden</button>
-      </form>
-  )
-}
-
-export default FormCheckout
+export default FormCheckout;
